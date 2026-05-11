8fefd601612145a92dc823ffffcf822020708159
jarvismack <mcdonaldzizu28@gmail.com>
2026-05-11 12:30:09 +0300

    fix ROOT_URLCONF in base settings

--- bodaboda_web/bodaboda_web/settings/base.py
+++ bodaboda_web/bodaboda_web/settings/base.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bodaboda_web.urls'
+ROOT_URLCONF = 'bodaboda_web.bodaboda_web.urls'
 
 TEMPLATES = [
     {
