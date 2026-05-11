4336a05a921e722a16a40dfffe0880d82c206522
jarvismack <mcdonaldzizu28@gmail.com>
2026-05-11 12:15:31 +0300

    fix settings module path

--- bodaboda_web/bodaboda_web/wsgi.py
+++ bodaboda_web/bodaboda_web/wsgi.py
@@ -1,6 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bodaboda_web.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'bodaboda_web.bodaboda_web.settings'
+)
 
 application = get_wsgi_application()
